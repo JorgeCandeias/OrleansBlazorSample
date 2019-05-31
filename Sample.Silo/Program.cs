8dd6ecf955d8ff4c7d358828369698550b956c60
Jorge Candeias <candeias@fastmail.fm>
2019-05-31 16:41:05 +0100

    Added silo project

---
+++ Sample.Silo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sample.Silo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
